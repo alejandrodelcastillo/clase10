705ed08bb779b2f0b13d70c086278c5ed059e194
alejandrodelcastillo <130861049+alejandrodelcastillo@users.noreply.github.com>
2023-05-20 01:14:04 -0300

    Tarea de la Clase 10
    trabajos con JS. correspondiente a la clase 10

---
+++ clase10/js/javascript.js
@@ -0,0 +1,31 @@
+var nombre = "Pedro";
+
+var apellido = "Herrera";
+
+
+console.log("Pedro"+" "+"Herrera");
+
+var numero1 = 8;
+var numero2 = 4;
+
+console.log(8+" "+4);
+
+let suma = 8 + 4;
+
+let nuevaVariable = suma + 30;
+console.log("suma"+" "+30);
+
+
+
+
+
+
+
+
+
+
+
+
+    
+
+
